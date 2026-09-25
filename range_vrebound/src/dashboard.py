cbee7e093a56da737f427ae221ab18cae511edd6
gatbatanic-eng <gatbatanic@gmail.com>
2026-09-25 21:57:52 +0900

    Add FPD research link to Golden Code navigation

--- range_vrebound/src/dashboard.py
+++ range_vrebound/src/dashboard.py
@@ -291,6 +291,7 @@ HTML_TEMPLATE = r"""<!doctype html>
       <a href="../technical/">기술적 신호</a>
       <a href="../momentum/">모멘텀 전략</a>
       <a href="../recovery/">계좌복구 공격매매</a>
+      <a href="../fpd/">FPD 연구</a>
     </div>
   </header>
 
